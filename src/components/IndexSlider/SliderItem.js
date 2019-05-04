5a4e8edf11cb32d8d651915d3fa8d275dd7a02d6
Alexis <alexis.oney@gmail.com>
2019-05-04 18:11:26 +0200

    Bugfix: srcset spelling

--- src/components/IndexSlider/SliderItem.js
+++ src/components/IndexSlider/SliderItem.js
@@ -13,12 +13,12 @@ export default props => {
         <picture>
           <source
             type='image/webp'
-            data-srcSet={`${src}-400w.webp 400w, ${src}-800w.webp 800w, ${src}-1600w.webp 1600w, ${src}-3200w.webp 3200w`}
+            data-srcset={`${src}-400w.webp 400w, ${src}-800w.webp 800w, ${src}-1600w.webp 1600w, ${src}-3200w.webp 3200w`}
             sizes='70vw'
           />
           <source
             type='image/jpeg'
-            data-srcSet={`${src}-400w.jpeg 400w, ${src}-800w.jpeg 800w, ${src}-1600w.jpeg 1600w`}
+            data-srcset={`${src}-400w.jpeg 400w, ${src}-800w.jpeg 800w, ${src}-1600w.jpeg 1600w`}
             sizes='70vw'
           />
           <img className='lazyload' data-src={`${src}-400w.jpeg`} alt={data.fields.slug} />
