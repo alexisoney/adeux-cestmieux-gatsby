72b637ca9f864a0b9537ca495dbd94124e650b57
Alexis <alexis.oney@gmail.com>
2019-05-04 15:44:00 +0200

    Adding lazyload to index slider

--- src/components/IndexSlider/SliderItem.js
+++ src/components/IndexSlider/SliderItem.js
@@ -13,15 +13,15 @@ export default props => {
         <picture>
           <source
             type='image/webp'
-            srcSet={`${src}-400w.webp 400w, ${src}-800w.webp 800w, ${src}-1600w.webp 1600w, ${src}-3200w.webp 3200w`}
+            data-srcSet={`${src}-400w.webp 400w, ${src}-800w.webp 800w, ${src}-1600w.webp 1600w, ${src}-3200w.webp 3200w`}
             sizes='70vw'
           />
           <source
             type='image/jpeg'
-            srcSet={`${src}-400w.jpeg 400w, ${src}-800w.jpeg 800w, ${src}-1600w.jpeg 1600w`}
+            data-srcSet={`${src}-400w.jpeg 400w, ${src}-800w.jpeg 800w, ${src}-1600w.jpeg 1600w`}
             sizes='70vw'
           />
-          <img src={`${src}-400w.jpeg`} alt={data.fields.slug} />
+          <img className='lazyload' data-src={`${src}-400w.jpeg`} alt={data.fields.slug} />
         </picture>
       </div>
       <div className='slider__date'>
