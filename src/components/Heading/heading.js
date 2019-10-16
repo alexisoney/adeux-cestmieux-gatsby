7da2ea4b2163615d82ccf7dc28d54abcc92715e6
Alexis <alexis.oney@gmail.com>
2019-10-16 19:12:29 +0200

    [ADD] Implement anchor for headings

--- src/components/Heading/heading.js
+++ src/components/Heading/heading.js
@@ -2,18 +2,30 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const Heading = ({text, level}) => {
+  let render = [];
+
   if (text) {
+    if (level === 'h2' || level === 'h3') {
+      render.push(
+        <div key='anchor' name={'foo'} data-testid='heading__anchor' className='heading__anchor' />
+      );
+    }
+
     switch (level) {
       case 'h2':
-        return <h2>{text}</h2>;
+        render.push(<h2 key='title'>{text}</h2>);
+        break;
       case 'h3':
-        return <h3>{text}</h3>;
+        render.push(<h3 key='title'>{text}</h3>);
+        break;
       case 'h4':
-        return <h4>{text}</h4>;
+        render.push(<h4 key='title'>{text}</h4>);
+        break;
       default:
     }
   }
-  return null;
+
+  return render.length > 0 ? render : null;
 };
 
 Heading.propTypes = {
--- src/components/Heading/heading.test.js
+++ src/components/Heading/heading.test.js
@@ -22,16 +22,34 @@ describe('The Heading component', () => {
 
   it('should return an H2 tag when level is h2', () => {
     const {container} = render(<Heading level='h2' text='foo' />);
-    expect(container.firstChild).toContainHTML('<h2>foo</h2>');
+    expect(container.querySelectorAll('h2')).toHaveLength(1);
   });
 
   it('should return an H3 tag when level is h3', () => {
     const {container} = render(<Heading level='h3' text='foo' />);
-    expect(container.firstChild).toContainHTML('<h3>foo</h3>');
+    expect(container.querySelectorAll('h3')).toHaveLength(1);
   });
 
   it('should return an H4 tag when level is h4', () => {
     const {container} = render(<Heading level='h4' text='foo' />);
-    expect(container.firstChild).toContainHTML('<h4>foo</h4>');
+    expect(container.querySelectorAll('h4')).toHaveLength(1);
+  });
+
+  it('should return an anchor tag for H2', () => {
+    const {queryAllByTestId, debug} = render(<Heading level='h2' text='foo' />);
+    const anchor = queryAllByTestId('heading__anchor');
+    expect(anchor).toHaveLength(1);
+  });
+
+  it('should return an anchor tag for H3', () => {
+    const {queryAllByTestId} = render(<Heading level='h3' text='foo' />);
+    const anchor = queryAllByTestId('heading__anchor');
+    expect(anchor).toHaveLength(1);
+  });
+
+  it('should NOT return an anchor tag for H4', () => {
+    const {queryByTestId} = render(<Heading level='h24' text='foo' />);
+    const anchor = queryByTestId('heading__anchor');
+    expect(anchor).toBe(null);
   });
 });
