80a9e85c1c789ecd16e01fa0dd2ed16b9954b68e
Alexis <alexis.oney@gmail.com>
2019-12-10 09:16:04 +0100

    Bind first_published_at

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const categories = require('./src/constant/categories');
 module.exports = Object.freeze({
   createStoryblokPageContext: function createStoryblokPageContext(node) {
     const category = node.parent_id;
-    const createdAt = node.created_at;
+    const createdAt = node.first_published_at || node.created_at;
     const slug = node.slug;
     const title = node.name;
     const content = typeof node.content === 'string' ? JSON.parse(node.content) : node.content;
